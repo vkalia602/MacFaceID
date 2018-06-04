ddf67bdab92b7ce9e94e73df4c24e7a3ebf4e6c2
Vasudha Kalia <vasudhakalia@Vasudhas-MacBook-Pro.local>
2018-06-03 17:58:39 -0700

    added dialog box function

--- training_data.py
+++ training_data.py
@@ -1,26 +1,38 @@
 #!/usr/bin/env python3
 import cv2
 from time import sleep
-from test import display_instruction
+import os
 cam = cv2.VideoCapture(0)
-
 cv2.namedWindow("test")
-
+integer = 0
+def dialog_box(instruction):
+    answer = """
+        osascript -e '
+            tell application "System Events"
+                set result to (display dialog "%s" giving up after 3)
+            end tell
+'
+        """ % (instruction)
+    os.system(answer)
 img_counter = 0
-instr_list = ["Face forward, look into the camera at 1 arm distance",
+instr_list = ["Let us collect some training data now",
+              "Face forward, look into the camera at 1 arm distance",
               "Face forward, now look at half arm distance",
-              "Move your face up", "Move your face down",
+              "Move your face up",
+              "Move your face down",
               "Move your face slight right",
               "Move your face slight left"]
 for instruction in instr_list:
-    display_instruction(instruction)
-    print("Press Space bar when ready")
+    dialog_box(instruction + "\nPlease press Space bar on Camera window when ready")
     while True:
         ret, frame = cam.read()
         cv2.imshow("test", frame)
         if not ret:
             break
         k = cv2.waitKey(1)
+#        if integer == 0:
+ #           integer += 1
+  #          break
         if k%256 == 32:
             # SPACE pressed
             for i in range(5):
@@ -30,7 +42,7 @@ for instruction in instr_list:
                 print("{} written!".format(img_name))
                 img_counter += 1
             break
-print("Training data has been recorded, Thank You")
+dialog_box("Training data has been recorded, Thank You")
 
 cam.release()
 
