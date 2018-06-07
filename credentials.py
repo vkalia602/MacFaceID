b8cf564c6157bc17aec36e7050d523a6f25e57a9
Vasudha Kalia <vasudhakalia@Vasudhas-MacBook-Pro.local>
2018-06-06 21:53:52 -0700

    Script that takes password and stores in keychain

--- credentials.py
+++ credentials.py
@@ -1,10 +1,35 @@
 #!/usr/bin/env python3
+import tkinter as tk
 import keyring
-import os
-
-
-"""
-cmd = osascript -e 'display dialog "Please enter a your login password." default answer "" with icon stop buttons {"Cancel", "Continue"} default button "Continue" with hidden answer'
-os.system(cmd)
-"""
-keyring.set_password("MacFace", "user", password)
+def make_entry(parent, caption, width=None, **options):
+    tk.Label(parent, text=caption).pack(side=tk.TOP)
+    entry = tk.Entry(parent, **options)
+    if width:
+        entry.config(width=width)
+    entry.pack(side=tk.TOP, padx=10, fill=tk.BOTH)
+    return entry
+def enter(event):
+    check_password()
+def check_password():
+    """ Collect 1's for every failure """
+    if (user.get() == password.get()):
+        keyring.set_password("MacFace", "user", password.get())        
+        root.title('Recorded')
+        return
+    else:
+        root.title('Try again')
+root = tk.Tk()
+root.geometry('300x160')
+root.title('Password Request')
+#frame for window margin
+parent = tk.Frame(root, padx=10, pady=10)
+parent.pack(fill=tk.BOTH, expand=True)
+#entrys with not shown text
+user = make_entry(parent, "Password:", 16, show='*')
+password = make_entry(parent, "Confirm Password:", 16, show="*")
+#button to attempt to login
+b = tk.Button(parent, borderwidth=4, text="OK", width=10, pady=8, command=check_password)
+b.pack(side=tk.BOTTOM)
+password.bind('<Return>', enter)
+user.focus_set()
+parent.mainloop()
